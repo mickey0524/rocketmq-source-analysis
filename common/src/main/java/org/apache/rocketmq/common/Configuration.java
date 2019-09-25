b471342c7c6c09fd930e2687013acc7a1b7992f4
mickey0524 <buptbh@163.com>
2019-09-25 20:15:26 +0800

    feat(namesrv): 新增 namesrv 模块

---
+++ common/src/main/java/org/apache/rocketmq/common/Configuration.java
@@ -0,0 +1,311 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.rocketmq.common;
+
+import org.apache.rocketmq.logging.InternalLogger;
+
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class Configuration {
+
+    private final InternalLogger log;  // Logger
+
+    private List<Object> configObjectList = new ArrayList<Object>(4);  // 存放 config 的 List
+    private String storePath;  // 存储的路径
+    private boolean storePathFromConfig = false;
+    private Object storePathObject;
+    private Field storePathField;
+    private DataVersion dataVersion = new DataVersion();
+    private ReadWriteLock readWriteLock = new ReentrantReadWriteLock();  // 读写锁
+    /**
+     * All properties include configs in object and extend properties.
+     */
+    private Properties allConfigs = new Properties();
+
+    // 传入 Logger
+    public Configuration(InternalLogger log) {
+        this.log = log;
+    }
+
+    // 传入 Logger 以及一个 Object 数组
+    public Configuration(InternalLogger log, Object... configObjects) {
+        this.log = log;
+        if (configObjects == null || configObjects.length == 0) {
+            return;
+        }
+        for (Object configObject : configObjects) {
+            registerConfig(configObject);
+        }
+    }
+
+    // 传入 Logger，storePath 以及 Object 数组
+    public Configuration(InternalLogger log, String storePath, Object... configObjects) {
+        this(log, configObjects);
+        this.storePath = storePath;
+    }
+
+    // 添加一个 Config
+    public Configuration registerConfig(Object configObject) {
+        try {
+            readWriteLock.writeLock().lockInterruptibly();
+
+            try {
+
+                Properties registerProps = MixAll.object2Properties(configObject);
+
+                merge(registerProps, this.allConfigs);
+
+                configObjectList.add(configObject);
+            } finally {
+                readWriteLock.writeLock().unlock();
+            }
+        } catch (InterruptedException e) {
+            log.error("registerConfig lock error");
+        }
+        return this;
+    }
+
+    // 直接 merge Properties
+    public Configuration registerConfig(Properties extProperties) {
+        if (extProperties == null) {
+            return this;
+        }
+
+        try {
+            readWriteLock.writeLock().lockInterruptibly();
+
+            try {
+                merge(extProperties, this.allConfigs);
+            } finally {
+                readWriteLock.writeLock().unlock();
+            }
+        } catch (InterruptedException e) {
+            log.error("register lock error. {}" + extProperties);
+        }
+
+        return this;
+    }
+
+    /**
+     * The store path will be gotten from the field of object.
+     * 存储路径从 object 中的 fieldName 字段获得
+     *
+     * @throws java.lang.RuntimeException if the field of object is not exist.
+     */
+    public void setStorePathFromConfig(Object object, String fieldName) {
+        assert object != null;
+        // 我觉得这用反射做不是很能理解。。。
+        // 直接在 NamesrvController 中调用函数获取，然后 set 一下 storePath 就行。。
+        try {
+            readWriteLock.writeLock().lockInterruptibly();
+
+            try {
+                this.storePathFromConfig = true;
+                this.storePathObject = object;
+                // check
+                this.storePathField = object.getClass().getDeclaredField(fieldName);
+                assert this.storePathField != null
+                    && !Modifier.isStatic(this.storePathField.getModifiers());
+                this.storePathField.setAccessible(true);
+            } catch (NoSuchFieldException e) {
+                throw new RuntimeException(e);
+            } finally {
+                readWriteLock.writeLock().unlock();
+            }
+        } catch (InterruptedException e) {
+            log.error("setStorePathFromConfig lock error");
+        }
+    }
+
+    // 获取存储路径，storePathField 优先级高
+    private String getStorePath() {
+        String realStorePath = null;
+        try {
+            readWriteLock.readLock().lockInterruptibly();
+
+            try {
+                realStorePath = this.storePath;
+
+                if (this.storePathFromConfig) {
+                    try {
+                        realStorePath = (String) storePathField.get(this.storePathObject);
+                    } catch (IllegalAccessException e) {
+                        log.error("getStorePath error, ", e);
+                    }
+                }
+            } finally {
+                readWriteLock.readLock().unlock();
+            }
+        } catch (InterruptedException e) {
+            log.error("getStorePath lock error");
+        }
+
+        return realStorePath;
+    }
+
+    // 设置存储路径
+    public void setStorePath(final String storePath) {
+        this.storePath = storePath;
+    }
+
+    // update 和 registerConfig 不一样，这里用的是 mergeIfExist
+    // 因为是 update，只能更新存在的 key
+    public void update(Properties properties) {
+        try {
+            readWriteLock.writeLock().lockInterruptibly();
+
+            try {
+                // the property must be exist when update
+                // key 必须存在
+                mergeIfExist(properties, this.allConfigs);
+
+                for (Object configObject : configObjectList) {
+                    // not allConfigs to update...
+                    // 还需要更新 ConfigList 的所有的 Config
+                    MixAll.properties2Object(properties, configObject);
+                }
+
+                this.dataVersion.nextVersion();
+
+            } finally {
+                readWriteLock.writeLock().unlock();
+            }
+        } catch (InterruptedException e) {
+            log.error("update lock error, {}", properties);
+            return;
+        }
+
+        persist();
+    }
+
+    // Config 持久化
+    public void persist() {
+        try {
+            readWriteLock.readLock().lockInterruptibly();
+
+            try {
+                String allConfigs = getAllConfigsInternal();
+
+                MixAll.string2File(allConfigs, getStorePath());
+            } catch (IOException e) {
+                log.error("persist string2File error, ", e);
+            } finally {
+                readWriteLock.readLock().unlock();
+            }
+        } catch (InterruptedException e) {
+            log.error("persist lock error");
+        }
+    }
+
+    public String getAllConfigsFormatString() {
+        try {
+            readWriteLock.readLock().lockInterruptibly();
+
+            try {
+
+                return getAllConfigsInternal();
+
+            } finally {
+                readWriteLock.readLock().unlock();
+            }
+        } catch (InterruptedException e) {
+            log.error("getAllConfigsFormatString lock error");
+        }
+
+        return null;
+    }
+
+    public String getDataVersionJson() {
+        return this.dataVersion.toJson();
+    }
+
+    public Properties getAllConfigs() {
+        try {
+            readWriteLock.readLock().lockInterruptibly();
+
+            try {
+
+                return this.allConfigs;
+
+            } finally {
+                readWriteLock.readLock().unlock();
+            }
+        } catch (InterruptedException e) {
+            log.error("getAllConfigs lock error");
+        }
+
+        return null;
+    }
+
+    // 获得所有的 Config
+    private String getAllConfigsInternal() {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        // reload from config object ?
+        for (Object configObject : this.configObjectList) {
+            Properties properties = MixAll.object2Properties(configObject);
+            if (properties != null) {
+                merge(properties, this.allConfigs);
+            } else {
+                log.warn("getAllConfigsInternal object2Properties is null, {}", configObject.getClass());
+            }
+        }
+
+        {
+            // allConfigs 中的 kv pair 组合起来
+            stringBuilder.append(MixAll.properties2String(this.allConfigs));
+        }
+
+        return stringBuilder.toString();
+    }
+
+    // merge Properties，将 from merge 到 to
+    private void merge(Properties from, Properties to) {
+        for (Object key : from.keySet()) {
+            Object fromObj = from.get(key), toObj = to.get(key);
+            // 如果 from 中的 key 在 to 中存在，且两个 value 不 equal，那么 log 记录
+            if (toObj != null && !toObj.equals(fromObj)) {
+                log.info("Replace, key: {}, value: {} -> {}", key, toObj, fromObj);
+            }
+            to.put(key, fromObj);
+        }
+    }
+
+    // 仅当 from 中的 key 在 to 中存在的时候，才 merge
+    private void mergeIfExist(Properties from, Properties to) {
+        for (Object key : from.keySet()) {
+            if (!to.containsKey(key)) {
+                continue;
+            }
+
+            Object fromObj = from.get(key), toObj = to.get(key);
+            if (toObj != null && !toObj.equals(fromObj)) {
+                log.info("Replace, key: {}, value: {} -> {}", key, toObj, fromObj);
+            }
+            to.put(key, fromObj);
+        }
+    }
+
+}
